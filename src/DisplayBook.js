a9fd2f66248a7a795ce20f0dbfc090dd15c035bf
Scott Litz <scott.litz@gmail.com>
2018-08-20 06:18:28 -0700

    Added a new component named DisplayBook to consolidate the code needed to display a single book.

---
+++ src/DisplayBook.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class DisplayBook extends Component {
+  static propTypes = {
+    book: PropTypes.object.isRequired
+  }
+
+  render () {
+    const { book } = this.props
+    return (
+      <li key={book.title}>
+        <div className="book">
+          <div className="book-top">
+            <div className='book-cover' style={{
+              width: `128px`,
+              height: `188px`,
+              backgroundImage: `url(${book.imageLinks.thumbnail})`
+            }}/>
+            <div className="book-shelf-changer">
+              <select>
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className='book-title'>
+            {book.title}
+          </div>
+          <div className='book-authors'>
+            {book.authors.map((author) =>
+            <span key={author}>{author}<br/></span>
+            )}
+          </div>
+        </div>
+      </li>
+    )
+  }
+}
+
+export default DisplayBook
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
+import DisplayBook from './DisplayBook'
 
 class SearchBooks extends Component {
   static propTypes = {
@@ -53,34 +54,9 @@ class SearchBooks extends Component {
           <div className="search-books-results">
             <ol className="books-grid">
               {displayedBooks.map((book) => (
-                <li key={book.title}>
-                  <div className='book'>
-                    <div className='book-top'>
-                      <div className='book-cover' style={{
-                        width: `128px`,
-                        height: `188px`,
-                        backgroundImage: `url(${book.imageLinks.thumbnail})`
-                      }}/>
-                      <div className="book-shelf-changer">
-                        <select>
-                          <option value="move" disabled>Move to...</option>
-                          <option value="currentlyReading">Currently Reading</option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className='book-title'>
-                      {book.title}
-                    </div>
-                    <div className='book-authors'>
-                      {book.authors.map((author) =>
-                        <span key={author}>{author}<br/></span>
-                      )}
-                    </div>
-                  </div>
-                </li>
+                <DisplayBook
+                  book={book}
+                />
               ))}
             </ol>
           </div>
